93f48989bf27b2c7caeca50ca41477b6338c5c0e
SashkoKeker <42063121+SashkoKeker@users.noreply.github.com>
2019-05-16 01:34:18 +0300

    ll

--- Proekt/Properties/Settings.Designer.cs
+++ Proekt/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace Proekt.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KEK\\KEKSQL;Initial Catalog=Proekt;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf" +
+            " ; Integrated Security=True")]
         public string Conect {
             get {
                 return ((string)(this["Conect"]));
